cfced87becf4d9d25d5d85041e9e7d92187af6e1
mjleffler <matthewleffler@gmail.com>
2016-03-03 19:45:43 -0500

    Create dibs_check.sh

---
+++ dibs_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get date for URL
+DATE=$(date +%Y-%m-%d)
+
+# URL to check
+URL=http://yourlibrary.evanced.info/dibsapi/reservations/$DATE/7
+
+# Temp files
+INPUT=~/dibs_input.tmp
+TEMP=~/check_dibs.tmp
+
+# Email address to be notified
+EMAIL=send@to.me
+
+# Get the page, remove opening line and diff feedback on that
+wget -q -O $INPUT "$URL" | jq '.' > $INPUT
+tr ',' '\n' < $INPUT | tr -d '"[]{}' | sed -i -e '$a\' $TEMP
+MODIFIED=$(diff ${TEMP} ${TEMP}.1)
+
+# Check if the modified variable exists and is more than 0 characters
+if [[ ! -z "$MODIFIED" && ${#MODIFIED} -gt 0 ]]; then
+    # Email new registrations
+    echo "$MODIFIED" #| ssmtp ${EMAIL}
+    # Copy current API pull to second file to compare next time
+    cp ${TEMP} ${TEMP}.1
+fi
