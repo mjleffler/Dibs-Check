c95169c13f66247b6fec3208898c8e20cb638f3c
mjleffler <matthewleffler@gmail.com>
2016-03-04 11:59:44 -0500

    Update dibs_check.sh
    Extended some comments and removed an unnecessary command.

--- dibs_check.sh
+++ dibs_check.sh
@@ -3,7 +3,7 @@
 # Get date for URL
 DATE=$(date +%Y-%m-%d)
 
-# URL to check
+# URL to check - edit to your library's Evanced address and correct room number at end
 URL=http://yourlibrary.evanced.info/dibsapi/reservations/$DATE/7
 
 # Temp files
@@ -14,14 +14,18 @@ TEMP=~/check_dibs.tmp
 EMAIL=send@to.me
 
 # Get the page, remove opening line and diff feedback on that
-wget -q -O $INPUT "$URL" | jq '.' > $INPUT
+wget -q -O $INPUT "$URL"
+
+# Replace commas with new lines | remove brackets, braces, and double quotes | add new line to end
 tr ',' '\n' < $INPUT | tr -d '"[]{}' | sed -i -e '$a\' $TEMP
+
+#Check for diffrences between new and old pages
 MODIFIED=$(diff ${TEMP} ${TEMP}.1)
 
 # Check if the modified variable exists and is more than 0 characters
 if [[ ! -z "$MODIFIED" && ${#MODIFIED} -gt 0 ]]; then
     # Email new registrations
-    echo "$MODIFIED" #| ssmtp ${EMAIL}
+    echo "$MODIFIED" | ssmtp ${EMAIL}
     # Copy current API pull to second file to compare next time
     cp ${TEMP} ${TEMP}.1
 fi
